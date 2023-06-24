5177ba44b8163934b2a8ca78131ac253d288871f
Makibaou0 <94782769+Makibaou0@users.noreply.github.com>
2023-06-24 13:30:52 +0700

    Update ActionResult.js

--- src/pages/ActionResult.js
+++ src/pages/ActionResult.js
@@ -2,17 +2,35 @@ import {View, Text} from 'react-native';
 import React, {useEffect} from 'react';
 import {Box} from 'native-base';
 import AnimatedLottieView from 'lottie-react-native';
+import axios from 'axios';
+import {storage} from '../utils/statis';
 const ActionResult = route => {
   const Navigation = route.route.navigation;
   const params = route.route.route.params;
   useEffect(() => {
-    setTimeout(() => {
-      Navigation.reset({
-        index: 0,
-        routes: [{name: 'Home'}],
+    GETALL();
+  });
+  const GETALL = async () => {
+    axios
+      .get('https://contact.herokuapp.com/contact')
+      .then(res => {
+        storage.set('CONTACT', JSON.stringify(res.data.data));
+        setTimeout(() => {
+          Navigation.reset({
+            index: 0,
+            routes: [{name: 'Home', params: res.data.data}],
+          });
+        }, 1000);
+      })
+      .catch(err => {
+        setTimeout(() => {
+          Navigation.reset({
+            index: 0,
+            routes: [{name: 'Home', params: []}],
+          });
+        }, 1000);
       });
-    }, 1500);
-  }, []);
+  };
   return (
     <Box flex={1}>
       {params == 'Success' ? (
